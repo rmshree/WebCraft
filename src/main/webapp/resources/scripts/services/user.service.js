09846125bc9fda153e5617149a5259d4087f065b
Manu SOdhi <mssodhi@ucdavis.edu>
2016-09-29 16:17:42 -0700

    adding user service

---
+++ src/main/webapp/resources/scripts/services/user.service.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('app').factory('UserService', function ($resource) {
+    return $resource('api/user/', {},  {
+        create: {method: 'GET', url: 'api/user/create/:name'}
+    });
+});
