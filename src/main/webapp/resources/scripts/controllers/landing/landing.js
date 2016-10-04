7eeab0cc9e248aacace673dd96e1e13121e4741f
Shawn Taxerman <TaxermanS@gmail.com>
2016-10-03 20:31:21 -0700

    Created basic front-end signup

--- src/main/webapp/resources/scripts/controllers/landing/landing.js
+++ src/main/webapp/resources/scripts/controllers/landing/landing.js
@@ -4,33 +4,29 @@ angular.module('app').controller('LandingCtrl', function (UserService) {
     var ctrl = this;
 
     ctrl.init = function () {
-        // making backend call
 
-        ctrl.name = 'Nitta';
+        ctrl.message = 'Welcome to Webcraft';
 
-        console.log('Called from init method on Landing page.');
-        ctrl.message = 'Welcome to Nittacraft a.out Edition';
+        //console.log('Called from init method on Landing page.');
+
 
     };
 
-    ctrl.printName = function (name) {
-        console.log(name);
+    ctrl.signUp = function (user) {
+        console.log(user);
         // UserService.create({name: ctrl.name}).$promise.then(function (response) {
         //     console.log(response);
         // })
     };
 
-    ctrl.clickSecondB = function () {
-        console.log(ctrl.date);
-        // var date = new Date();
-        // console.log(date);
-    };
 
-    ctrl.isDisabled = function () {
 
-        if(ctrl.name){
+    /*ctrl.isDisabled = function () {
+
+        if(ctrl.user.name && ctrl.user.username  && ctrl.user.password ){
             return true;
         }
     }
+    */
 
 });
