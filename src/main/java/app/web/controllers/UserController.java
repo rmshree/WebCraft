b5e9fb743e77b6d3fb44537b3734000ea83da6bc
Eric Nghia Nguyen <neanguyen@ucdavis.edu>
2016-10-05 01:23:08 -0700

    Removed accidental line of code.

--- src/main/java/app/web/controllers/UserController.java
+++ src/main/java/app/web/controllers/UserController.java
@@ -39,7 +39,6 @@ public class UserController {
         if (user == null){
             User newUser = new User();
             newUser.setUsername(username);
-            DatatypeConverter.parseBase64Binary(userDetails.getPassword());
             newUser.setPassword(DatatypeConverter.printBase64Binary(userDetails.getPassword().getBytes()));
             newUser.setEmail("MockEmail@ECS160.edu");
             newUser.setLoss(0);
