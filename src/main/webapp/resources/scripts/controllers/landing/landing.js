abaf153fcaac04d2015d8e9c5e25ebfa2c84cfe2
Shawn Taxerman <TaxermanS@gmail.com>
2016-10-04 18:13:55 -0700

    Fixed bug in landing.js signup function

--- src/main/webapp/resources/scripts/controllers/landing/landing.js
+++ src/main/webapp/resources/scripts/controllers/landing/landing.js
@@ -13,14 +13,16 @@ angular.module('app').controller('LandingCtrl', function (UserService) {
         console.log(user);
 
         UserService.getUserByUsername({username: user.username}).$promise.then(function (response) {
+
             console.log(response);
 
             //if response is null, create a new user
-            if (response.id === null && response.username !== user.username) {
+            if (typeof response.id === "undefined") {
                 UserService.createNewUser({username: user.username}, user).$promise.then(function (response) {
                     console.log(response);
                 });
             }//end if
+            else console.log("User already exists!");
           });
         };
     });
