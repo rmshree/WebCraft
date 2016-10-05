e32094bc9156f5516e59196c3e31d40dee016f67
Shawn Taxerman <TaxermanS@gmail.com>
2016-10-04 17:46:44 -0700

    Updated GetUserByUsername()

--- src/main/webapp/resources/scripts/controllers/landing/landing.js
+++ src/main/webapp/resources/scripts/controllers/landing/landing.js
@@ -11,12 +11,17 @@ angular.module('app').controller('LandingCtrl', function (UserService) {
 
     ctrl.signUp = function (user) {
         console.log(user);
-        UserService.createNewUser({username: user.username}, user).$promise.then(function (response) {
-            console.log(response);
-        });
-        UserService.getUserByUsername({username: user.username}).$promise.then(function (response) {
-            console.log(response)
-        });
-    };
 
-});
+        UserService.getUserByUsername({username: user.username}).$promise.then(function (response) {
+            console.log(response);
+
+            //if response is null, create a new user
+            if (response.id === null && response.username !== user.username) {
+                UserService.createNewUser({username: user.username}, user).$promise.then(function (response) {
+                    console.log(response);
+                });
+            }//end if
+          });
+        };
+    });
+
