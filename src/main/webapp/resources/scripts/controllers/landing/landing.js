133cf62dc46942c6c0314b466457da4ae1e444bb
Manu SOdhi <mssodhi@ucdavis.edu>
2016-10-04 17:06:51 -0700

    making changes to signup apis

--- src/main/webapp/resources/scripts/controllers/landing/landing.js
+++ src/main/webapp/resources/scripts/controllers/landing/landing.js
@@ -7,29 +7,16 @@ angular.module('app').controller('LandingCtrl', function (UserService) {
 
         ctrl.message = 'Welcome to Webcraft';
 
-        //console.log('Called from init method on Landing page.');
-
-
     };
 
     ctrl.signUp = function (user) {
         console.log(user);
-        UserService.createNewUser({name: user.username}, {password: user.password}).$promise.then(function (response) {
+        UserService.createNewUser({username: user.username}, user).$promise.then(function (response) {
             console.log(response);
         });
-        UserService.getUserByUsername({name: user.username}).$promise.then(function (response) {
+        UserService.getUserByUsername({username: user.username}).$promise.then(function (response) {
             console.log(response)
         });
     };
-
-
-
-    /*ctrl.isDisabled = function () {
-
-        if(ctrl.user.name && ctrl.user.username  && ctrl.user.password ){
-            return true;
-        }
-    }
-    */
 
 });
--- src/main/webapp/resources/scripts/services/user.service.js
+++ src/main/webapp/resources/scripts/services/user.service.js
@@ -4,9 +4,8 @@ angular.module('app').factory('UserService', function ($resource) {
 
     return $resource('api/user/', {},  {
 
-        getUserByUsername: {method: 'GET', url: 'api/user/check/:name'},
-
-        createNewUser: {method: 'PUT', url: 'api/user/create/:name'},
+        getUserByUsername: {method: 'GET', url: 'api/user/get/:username'},
+        createNewUser: {method: 'PUT', url: 'api/user/create/:username'}
 
     });
 });
