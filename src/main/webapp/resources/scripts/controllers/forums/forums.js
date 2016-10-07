415c325705f3910e0c1088b6b043baef25e2a3de
Manu SOdhi <mssodhi@ucdavis.edu>
2016-10-07 00:52:38 -0700

    user can add their own custom form text and title

--- src/main/webapp/resources/scripts/controllers/forums/forums.js
+++ src/main/webapp/resources/scripts/controllers/forums/forums.js
@@ -1,24 +1,24 @@
 'use strict';
 
-angular.module('app').controller('ForumsCtrl', function ($location, ForumsService) {
+angular.module('app').controller('ForumsCtrl', function (ForumsService) {
     var ctrl = this;
 
     ctrl.init = function () {
         ForumsService.getAll().$promise.then(function (response) {
             ctrl.posts = response;
-            console.log(response);
+            console.log('All forums', response);
         });
     };
 
-    ctrl.post = function () {
-
-        var post = {
-            title: 'Test',
-            text: 'lled any time the html is rendered by the browser '
-        };
-
-        ForumsService.add(post).$promise.then(function (response) {
-            console.log(response);
-        });
+    ctrl.post = function (newPost) {
+        if (newPost.title && newPost.text) {
+            ForumsService.add(newPost).$promise.then(function (response) {
+                console.log(response);
+                if (response.id) {
+                    ctrl.showForm = false;
+                    ctrl.posts.push(response);
+                }
+            });
+        }
     };
 });
