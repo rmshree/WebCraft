60526bad617b137d4a3e9d412f0ca1f41dfc6065
Shawn Taxerman <TaxermanS@gmail.com>
2016-10-09 16:47:28 -0700

    Merge branch 'master' into feat-Cookies Added CookieService Implementation

--- src/main/java/app/web/services/CookieService.java
+++ src/main/java/app/web/services/CookieService.java
@@ -1,53 +1,13 @@
 package app.web.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import java.util.List;
-import app.web.domain.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-import javax.crypto.spec.SecretKeySpec;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.xml.bind.DatatypeConverter;
-import java.security.Key;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
+public interface CookieService {
 
-import static org.apache.taglibs.standard.lang.jstl.StringLiteral.getValueFromToken;
 
-@Service
-public class CookieService {
+    void setCurrentUser();
 
-    @Autowired
-    private HttpServletRequest request;
+    String getValueFromCookie();
 
-    @Autowired
-    private HttpServletResponse response;
-
-    public void setCurrentUser(){
-        Cookie cookie = new Cookie("testCookie", "testValue");
-        cookie.setMaxAge(1000000);
-        cookie.setPath("/");
-        response.addCookie(cookie);
-    }
-
-    public String getValueFromCookie() {
-        Cookie[] cookies = request.getCookies();
-        String value = "";
-        if (cookies != null) {
-            for(Cookie cookie: cookies){
-                if(cookie.getName().equalsIgnoreCase("testCookie")) {
-                    value = cookie.getValue();
-                    break;
-                }
-            }
-        }
-        return value;
-    }
 
 }
 
