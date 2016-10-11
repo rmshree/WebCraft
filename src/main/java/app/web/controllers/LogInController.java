98649bb71b5135a3f4a81e085001add1406a32ee
mzhrby <mracristobal@ucdavis.edu>
2016-10-10 20:33:36 -0600

    added login controller api along with logout

---
+++ src/main/java/app/web/controllers/LogInController.java
@@ -0,0 +1,7 @@
+package app.web.controllers;
+
+/**
+ * Created by misht on 10/10/2016.
+ */
+public class LogInController {
+}
---
+++ src/main/java/app/web/services/LogInService.java
@@ -0,0 +1,7 @@
+package app.web.services;
+
+/**
+ * Created by misht on 10/10/2016.
+ */
+public interface LogInService {
+}
