10338f53acbe8e26a6486022a8433f3d926b37c1
Jessi Bhadra <jessibhadra@gmail.com>
2016-10-12 20:08:26 -0700

    new way to upload file

--- src/main/java/app/web/services/FileArchiveService.java
+++ src/main/java/app/web/services/FileArchiveService.java
@@ -1,8 +1,8 @@
 package app.web.services;
 
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
@@ -10,6 +10,8 @@ import java.net.URL;
 public interface FileArchiveService {
 
     URL uploadFile(MultipartFile m_fileToUpload, String key) throws IOException;
+
+    String uploadFile2(MultipartFile m_fileToUpload, String key, ObjectMetadata meta) throws IOException;
 
     Object getFile(URL userURL) throws IOException;
 
