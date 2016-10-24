b51e3c69f67443ca7b7ab83e2e2370ae39dd7268
Farzin Shargh <fshargh@ucdavis.edu>
2016-10-23 19:33:23 -0700

    Starting to write the Controllers and Services

--- src/main/java/app/web/controllers/MessagingController.java
+++ src/main/java/app/web/controllers/MessagingController.java
@@ -1,4 +1,28 @@
+/** \file MessagingController.java
+ * Back-End Messaging services that are used by Front-End.
+ * Called by using /api/messaging/
+ */
+
 package app.web.controllers;
 
+import app.web.domain.Conversation;
+import app.web.domain.Message;
+import app.web.services.ConversationService;
+import app.web.services.MessageService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/messaging/")
 public class MessagingController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @Autowired
+    private ConversationService conversationService;
+
 }
