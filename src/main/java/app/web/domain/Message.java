d8582a02429b9de7cad2b1a9c6e555e990987ecf
Jessi Bhadra <jessibhadra@gmail.com>
2016-10-23 17:56:22 -0700

    Added more to message entity

--- src/main/java/app/web/domain/Message.java
+++ src/main/java/app/web/domain/Message.java
@@ -25,18 +25,74 @@ public class Message implements Serializable{
     private String message_body;
 
     @Column(name = "create_date", nullable = false)
-            @JsonProperty
-            private Date create_date = new Date();
+    @JsonProperty
+    private Date create_date = new Date();
 
-            @Column(name = "sender", nullable = false)
-            @JsonProperty
-            private User sender;
+    @Column(name = "sender", nullable = false)
+    @JsonProperty
+    private User sender;
 
-            @Column(name = "receiver", nullable = false)
-            @JsonProperty
-            private User receiver;
+    @Column(name = "receiver", nullable = false)
+    @JsonProperty
+    private User receiver;
 
-            @Column(name = "read")
-            @JsonProperty
-            private boolean read;
+    @Column(name = "read")
+    @JsonProperty
+    private boolean read;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getConvo_id() {
+        return convo_id;
+    }
+
+    public void setConvo_id(Integer convo_id) {
+        this.convo_id = convo_id;
+    }
+
+    public String getMessage_body() {
+        return message_body;
+    }
+
+    public void setMessage_body(String message_body) {
+        this.message_body = message_body;
+    }
+
+    public Date getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(Date create_date) {
+        this.create_date = create_date;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
 }
