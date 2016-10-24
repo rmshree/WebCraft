a6a40a7e2c86446a60d9bde287bfa52005707191
Jessi Bhadra <jessibhadra@gmail.com>
2016-10-23 19:31:29 -0700

    Minor fixes

--- src/main/java/app/web/services/ConversationService.java
+++ src/main/java/app/web/services/ConversationService.java
@@ -1,4 +1,7 @@
 package app.web.services;
 
+/**
+ * Created by jessibhadra on 10/23/16.
+ */
 public interface ConversationService {
 }
--- src/main/java/app/web/services/MessageService.java
+++ src/main/java/app/web/services/MessageService.java
@@ -1,4 +1,14 @@
 package app.web.services;
 
+import app.web.domain.Message;
+
+import java.util.List;
+
 public interface MessageService {
+    Message save(Message message);
+
+    Message getMessagetByID(Integer id);
+
+    List<Message> getMessageByConvo(Integer id);
+
 }
