e7d7e62229434cd29e1862274e62c141f1682733
Jessi Bhadra <jessibhadra@gmail.com>
2016-10-24 08:59:08 -0700

    Modified query in conversation repository

--- src/main/java/app/web/data/ConversationRepository.java
+++ src/main/java/app/web/data/ConversationRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ConversationRepository extends JpaRepository<Conversation, User>{
-    @Query("select c from Conversation c where c.user1 = ?1 and c.user2 = ?2")
+    @Query("select c from Conversation c where c.user1 = ?1 and c.user2 = ?2 or c.user1 = ?2 and c.user2 = ?1 ")
     Conversation getConvobyUsers(User user1, User user2);
 
     @Query("select c from Conversation c where c.user1 = ?1 or c.user2 = ?1")
--- src/main/java/app/web/services/MessageServicelmpl.java
+++ src/main/java/app/web/services/MessageServicelmpl.java
@@ -3,9 +3,12 @@ package app.web.services;
 import app.web.data.MessageRepository;
 import app.web.domain.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
+@Service
 public class MessageServicelmpl implements MessageService {
     @Autowired
     private MessageRepository messageRepository;
