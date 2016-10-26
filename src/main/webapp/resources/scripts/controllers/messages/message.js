5eba9e0fa0bd1e542210a6136d3ea2ce215228fa
Shawn Taxerman <TaxermanS@gmail.com>
2016-10-25 21:03:04 -0700

    Began creating a messaging front-end

--- src/main/webapp/resources/scripts/controllers/messages/message.js
+++ src/main/webapp/resources/scripts/controllers/messages/message.js
@@ -1,8 +1,11 @@
 'use strict';
 
-angular.module('app').controller('MessagesCtrl', function (currentUser, MessageService) {
+angular.module('app').controller('MessagesCtrl', function (currentUser, MessageService, UserService) {
     var ctrl = this;
     ctrl.currentUser = currentUser;
+    ctrl.toUser = null;
+    ctrl.isSendingMessage = false;
+    ctrl.statusMessage = '';
 
     ctrl.init = function () {
         ctrl.getMessages();
@@ -13,6 +16,33 @@ angular.module('app').controller('MessagesCtrl', function (currentUser, MessageS
             console.log(response);
             ctrl.messages = response;
         })
+    };
+
+    ctrl.sendMessage = function(newMessage) {
+
+        UserService.getUserByUsername(newMessage.receiver).$promise.then(function (response) {
+            if (response.id) {
+                ctrl.toUser = response;
+                var message = {
+                    message_body: newMessage.body,
+                    sender: ctrl.currentUser,
+                    receiver: ctrl.toUser
+                }
+
+                console.log(message);
+                MessageService.sendMessage(message).$promise.then(function (response) {
+                    if (response.id) {
+                        ctrl.isSendingMessage = false;
+                        ctrl.messages.push(response); //SHOULD REMOVE THIS LATER
+                        console.log(response);
+                    }
+                });
+            }
+            else {
+                //receiver does not exist
+                ctrl.statusMessage = "User" + newMessage.receiver + " does not exits.";
+            }
+        });
     };
 
 
