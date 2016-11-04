675b89e285b9480309f4d3740277b8e63a3c17c9
Shawn Taxerman <TaxermanS@gmail.com>
2016-11-04 14:00:42 -0700

    Implemented JWT User Authentication

--- src/main/java/app/web/services/CookieServiceImpl.java
+++ src/main/java/app/web/services/CookieServiceImpl.java
@@ -1,5 +1,6 @@
 package app.web.services;
 
+import com.amazonaws.services.dynamodbv2.xspec.N;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.spec.SecretKeySpec;
+import io.jsonwebtoken.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,18 +30,28 @@ public class CookieServiceImpl implements CookieService {
     @Autowired
     private HttpServletResponse response;
 
+    /*Declare the secret key, which is defined in app.*.properties file */
+    @Value("${jwt.io.secret}")
+    private String secretKey;
+
     private static final Integer SECONDS_IN_A_DAY = 84000;
 
     @Override
-    public void setCurrentUser(User user){
-        String payload = "";
-        if(user != null){
-            payload = user.getUsername();
+    public void setCurrentUser(User user) {
+
+        if (user != null) {
+            Cookie cookie = new Cookie("user_cookie", createSecretToken(user));
+            cookie.setMaxAge(SECONDS_IN_A_DAY * 2);
+            cookie.setPath("/");
+            response.addCookie(cookie);
         }
-        Cookie cookie = new Cookie("user_cookie", payload);
-        cookie.setMaxAge(SECONDS_IN_A_DAY * 2);
-        cookie.setPath("/");
-        response.addCookie(cookie);
+        else { //user is null, i.e logging out
+            Cookie cookie = new Cookie("user_cookie", null);
+            cookie.setMaxAge(1000);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+        }
+
     }
 
     @Override
@@ -49,12 +61,59 @@ public class CookieServiceImpl implements CookieService {
         if (cookies != null) {
             for(Cookie cookie: cookies){
                 if(cookie.getName().equalsIgnoreCase("user_cookie")) {
-                    value = cookie.getValue();
+                    value = getValueFromToken(cookie.getValue());
                     break;
                 }
             }
         }
         return value;
+    }
+
+
+    private String createSecretToken(User user) {
+
+        //JWT Algorithm used to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        //Signing the algorithm with our own secret key
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secretKey);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+
+        String theirUsername = "";
+        if (user.getId() != null) {
+            theirUsername = user.getUsername();
+        }
+
+        //Set the JWT Claims
+        String issuer = "SYSTEM";
+        String subject= "Logging In";
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        long expMillis = nowMillis + (TimeUnit.DAYS.toMillis(1)); //Set expiration date to 1 day in the future
+        Date exp = new Date(expMillis);
+
+        JwtBuilder builder = Jwts.builder().setId(theirUsername)
+                .setIssuedAt(now)
+                .setExpiration(exp)
+                .setSubject(subject)
+                .setIssuer(issuer)
+                .signWith(signatureAlgorithm, signingKey);
+
+        //Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+
+    }
+
+    private String getValueFromToken(String token){
+        try{
+            Claims claims = Jwts.parser()
+                    .setSigningKey(DatatypeConverter.parseBase64Binary(secretKey))
+                    .parseClaimsJws(token).getBody();
+            return claims.getId();
+        }catch (Exception e){
+            return null;
+        }
     }
 
 }
