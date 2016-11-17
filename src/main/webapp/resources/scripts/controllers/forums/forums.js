47bb308035beab163b415a6876c0d7a5d62eef54
Shawn Taxerman <TaxermanS@gmail.com>
2016-11-16 18:48:33 -0800

    Added forums.js

---
+++ src/main/webapp/resources/scripts/controllers/forums/forums.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('app').controller('forumsCtrl', function ($location, ForumsService) {
+    var ctrl = this;
+
+    ctrl.init = function () {
+        ctrl.GeneralDiscussion = 1;
+        ctrl.UserGuides = 2;
+        ctrl.TechnicalSupport = 3;
+        ctrl.Miscellaneous = 4;
+    }
+
+    ctrl.goToForum = function (category) {
+        window.location.href = "#/forums/" + category;
+    };
+});
